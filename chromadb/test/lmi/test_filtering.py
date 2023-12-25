7fc96e78f0915df7df1fe9dd8f3dfc7fb8332136
Jakub Zovak <jakub.zovak@datasentics.com>
2023-12-26 00:34:10 +0100

    Multilevel filtering for LMI DONE

--- chromadb/test/lmi/test_filtering.py
+++ chromadb/test/lmi/test_filtering.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pytest
-from chromadb.li_index.search.attribtue_filtering.default_filtering import precompute_bucket_ids, compute_ratios_for_attribute_filters, combine_probabilities
+from chromadb.li_index.search.attribtue_filtering.default_filtering import (precompute_bucket_ids,
+                                                                            compute_ratios_for_attribute_filters,
+                                                                            combine_probabilities,
+                                                                            path_children_from_categories,
+                                                                            get_children_probabilities)
 
 def test_precompute_bucket_ids():
     n_categories = [2, 2]
@@ -27,5 +31,35 @@ def test_combine_probabilities():
 
     # Perform the test
     output = combine_probabilities(input_ratios)
-    print("output", output)
     assert output == expected_output
+
+def test_path_children_from_categories_1():
+    path = (0, -1, -1)
+    categories = [[1, 0], [0, 1]]
+    expected_output = [[(0, 1, -1), (0, 0, -1)], [(0, 0, -1), (0, 1, -1)]]
+
+    # Perform the test
+    output = path_children_from_categories(path, categories)
+    assert output == expected_output
+
+def test_path_children_from_categories_2():
+    path = (0, -1)
+    categories = [[1, 0], [0, 1]]
+    expected_output = [[(0, 1), (0, 0)], [(0, 0), (0, 1)]]
+
+    # Perform the test
+    output = path_children_from_categories(path, categories)
+    assert output == expected_output
+
+
+def test_get_children_probabilities():
+    path_children = [[(0, 1), (0, 0)], [(0, 0), (0, 1)]]
+    constraint_tree_ratios = [
+        {(0, 0): 0.0, (0, 1): 1.0, (1, 0): 0.0, (1, 1): 0.0, (-1, -1): 1.0, (0, -1): 1.0, (1, -1): 0.0},
+        {(0, 0): 0.5, (0, 1): 0.5, (1, 0): 0.0, (1, 1): 0.0, (-1, -1): 1.0, (0, -1): 1.0, (1, -1): 0.0}]
+
+    expected_output = np.array([[1.0, 0.0], [0.5, 0.5]])
+
+    # Perform the test
+    output = get_children_probabilities(path_children, constraint_tree_ratios)
+    assert np.array_equal(output,expected_output)
